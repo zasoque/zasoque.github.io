1624e6d5a2e014aa75fab3e31702b59ee886e17b
Sch <junhg0211@gmail.com>
2026-04-24 17:31:08 +0900

    Add new language tools and resources links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,4 +8,13 @@
 	<li><a href="https://zasoque.org/glossing">행간주석 도구</a></li>
 	<li><a href="https://zasoque.org/dictform">단어장 도구</a></li>
 	<li><a href="https://aligner.tinygods.dev/">단어별 번역 시각화 도구</a></li>
+	<li><a href="https://astianthus.github.io/sophoi/">IPA 풀 차트</a></li>
+	<li><a href="https://defseg.io/psmith/">언어 인벤토리 검색 사이트</a></li>
+	<li><a href="https://fontdrop.info">폰트 파일 미리보기 사이트</a></li>
+	<li><a href="https://readingglosses.com/">행간주석 공부 블로그</a></li>
+	<li><a href="https://lexicanter.com">Lexicanter 인공어 단어장 프로그램</a></li>
+	<li><a href="https://draquet.github.io/PolyGlot/">PolyGlot 인공어 관리 프로그램</a></li>
+	<li><a href="https://www.lexurgy.com/">음운변동 시뮬레이터</a></li>
+	<li><a href="https://www.kratylos.org/~raphael/claw/phonomaton.cgi">음운변동 시뮬레이터 2</a></li>
+	<li><a href="https://www.wordfrequency.info/samples.asp">단어 빈도 분석 데이터</a></li>
 </ul>
