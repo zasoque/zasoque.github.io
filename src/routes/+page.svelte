4954581c0ae534a49dcc277978139b87c4fd597f
Sch <junhg0211@gmail.com>
2026-04-24 10:43:39 +0900

    Update links to external sites in page component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <div>자소크어 도구</div>
 <ul>
-	<li><a href="/tion">자소크어 사전</a></li>
-	<li><a href="/comcosifin">자소크어 작문 도구</a></li>
+	<li><a href="https://zasoque.org/tion">자소크어 사전</a></li>
+	<li><a href="https://zasoque.org/comcosifin">자소크어 작문 도구</a></li>
 </ul>
 <div>언어학 도구</div>
 <ul>
-	<li><a href="/glossing">행간주석 도구</a></li>
-	<li><a href="/dictform">단어장 도구</a></li>
+	<li><a href="https://zasoque.org/glossing">행간주석 도구</a></li>
+	<li><a href="https://zasoque.org/dictform">단어장 도구</a></li>
 	<li><a href="https://aligner.tinygods.dev/">단어별 번역 시각화 도구</a></li>
 </ul>
