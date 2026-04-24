1301b926063957304fa3cceaa06bcea3ecc13c06
Sch <junhg0211@gmail.com>
2026-04-24 10:41:27 +0900

    Update page content and links for 자소크.org

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
-<div class="title">자소크.org</div>
-
-<style>
-	.title {
-		font-size: 2em;
-		font-weight: bold;
-		color: #333;
-		text-align: center;
-	}
-</style>
+<div>자소크어 도구</div>
+<ul>
+	<li><a href="/tion">자소크어 사전</a></li>
+	<li><a href="/comcosifin">자소크어 작문 도구</a></li>
+</ul>
+<div>언어학 도구</div>
+<ul>
+	<li><a href="/glossing">행간주석 도구</a></li>
+	<li><a href="/dictform">단어장 도구</a></li>
+	<li><a href="https://aligner.tinygods.dev/">단어별 번역 시각화 도구</a></li>
+</ul>
