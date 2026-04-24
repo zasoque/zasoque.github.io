6e951eeda13e5e95064178445f042afcdb586038
Sch <junhg0211@gmail.com>
2026-04-24 21:54:22 +0900

    Update language tools and resources in Svelte component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,25 @@
 	<li><a href="https://zasoque.org/tion">자소크어 사전</a></li>
 	<li><a href="https://zasoque.org/comcosifin">자소크어 작문 도구</a></li>
 </ul>
-<div>언어학 도구</div>
+<div>인공언어 도구</div>
 <ul>
+	<li><a href="https://astianthus.github.io/sophoi/">IPA 풀 차트</a></li>
+	<li><a href="https://fontdrop.info">폰트 파일 미리보기 사이트</a></li>
 	<li><a href="https://zasoque.org/glossing">행간주석 도구</a></li>
 	<li><a href="https://zasoque.org/dictform">단어장 도구</a></li>
 	<li><a href="https://aligner.tinygods.dev/">단어별 번역 시각화 도구</a></li>
-	<li><a href="https://astianthus.github.io/sophoi/">IPA 풀 차트</a></li>
-	<li><a href="https://defseg.io/psmith/">언어 인벤토리 검색 사이트</a></li>
-	<li><a href="https://fontdrop.info">폰트 파일 미리보기 사이트</a></li>
-	<li><a href="https://readingglosses.com/">행간주석 공부 블로그</a></li>
 	<li><a href="https://lexicanter.com">Lexicanter 인공어 단어장 프로그램</a></li>
 	<li><a href="https://draquet.github.io/PolyGlot/">PolyGlot 인공어 관리 프로그램</a></li>
 	<li><a href="https://www.lexurgy.com/">음운변동 시뮬레이터</a></li>
 	<li><a href="https://www.kratylos.org/~raphael/claw/phonomaton.cgi">음운변동 시뮬레이터 2</a></li>
+</ul>
+<div>기성언어학 도구</div>
+<ul>
+	<li><a href="https://defseg.io/psmith/">언어 인벤토리 검색 사이트</a></li>
+	<li><a href="https://readingglosses.com/">행간주석 공부 블로그</a></li>
 	<li><a href="https://www.wordfrequency.info/samples.asp">단어 빈도 분석 데이터</a></li>
+</ul>
+<div>유용한 도구</div>
+<ul>
+	<li><a href="https://zasoque.org/speed-reader">속독기</a></li>
 </ul>
